fe491e425e128e679b03e3d22270a13234fed69c
CadeReinberger <williamcadereinberger@gmail.com>
2026-02-12 13:23:39 -0500

    Okay maybe I fixed the writing bug

--- run.py
+++ run.py
@@ -7,10 +7,10 @@ def solve_full(start_drop, prob_univ):
     out_t, out_x = solve_problem(start_drop, prob_univ)
 
     # Now, we pickle the setup
-    pickle_setup(start_drop, prob_univ, out_file = sps.OUTPUT_DIR + 'setup.pkl')
+    pickle_setup(start_drop, prob_univ, out_file = prob_univ.sol_ps.OUTPUT_DIR + 'setup.pkl')
 
     # Next, we pickle the problem
-    pickle_output(out_t, out_x, sps.OUTPUT_DIR + 'results.pkl')
+    pickle_output(out_t, out_x, prob_univ.sol_ps.OUTPUT_DIR + 'results.pkl')
 
     # Let's print something big here
     print('\n' * 5)
